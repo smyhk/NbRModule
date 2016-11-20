f1daf798c62e0f8840fa3c51887735b0e1b3fa75
Steve Kedzie <seal.team.smyhk@gmail.com>
2016-11-20 08:26:57 -0800

    Create executable actions

--- ROptions/src/org/teamtwo/r/toolbar/RActionListener.java
+++ ROptions/src/org/teamtwo/r/toolbar/RActionListener.java
@@ -7,10 +7,16 @@ package org.teamtwo.r.toolbar;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.List;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
+import org.openide.loaders.DataObject;
 import org.openide.util.NbBundle.Messages;
+import org.openide.util.NbPreferences;
+import org.openide.util.RequestProcessor;
+import org.teamtwo.r.options.RPanel;
 
 @ActionID(
         category = "File",
@@ -24,10 +30,27 @@ import org.openide.util.NbBundle.Messages;
 @Messages("CTL_RActionListener=RAction")
 public final class RActionListener implements ActionListener
 {
+    private final List<DataObject> context;
+    private RequestProcessor processor = null;
+
+    public RActionListener(List<DataObject> context) {
+        this.context = context;
+    }
+
 
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        // TODO implement action body
+        String executableFilePath = NbPreferences.forModule(RPanel.class).get("RPanel.path", "");
+        File file = new File(executableFilePath);
+
+        for (DataObject dataObject : context) {
+            processor = new RequestProcessor("Exec", 1, true);
+            if ("Rscript".equals(file.getName())) {
+                processor.execute(new ReditorRunnable(dataObject, ""));
+            } else {
+                processor.execute(new ReditorRunnable(dataObject, " -q -f "));
+            }
+        }
     }
 }
